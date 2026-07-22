619bd4e47234a8b26b6e4fe02a3ec3e264803d9f
weizhifeng <>
2026-07-22 22:21:18 +0800

    feat: treeshaking的使用

--- src/index/index.js
+++ src/index/index.js
@@ -4,10 +4,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import logo from "../assets/icon_150x150.png";
 import "./search.less";
+import callHello from "./tree-shake";
 
 class Search extends React.Component {
   render() {
-    debugger;
+    callHello();
     return (
       <div className="search-text">
         Search Text
